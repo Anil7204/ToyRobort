9861e8f37223807979714286d5647fee91d71d2e
Anil7204 <101301043+Anil7204@users.noreply.github.com>
2022-03-10 16:07:18 +1000

    Add files via upload

---
+++ Behaviour.cs
@@ -0,0 +1,68 @@
+﻿
+namespace ToyRobort
+{
+    /// <summary>
+    /// This class is used to simulate the behaviour of the toy.
+    /// </summary>
+    public class Behaviour
+    {
+        public ToyRobot ToyRobot { get; private set; }
+        public ToyBoard SquareBoard { get; private set; }
+        public InputParser InputParser { get; private set; }
+        public static string PlaceDirection { get; set; }
+        public Behaviour(ToyRobot toyRobot, ToyBoard squareBoard, InputParser inputParser)
+        {
+            ToyRobot = toyRobot;
+            SquareBoard = squareBoard;
+            InputParser = inputParser;
+        }
+
+        public string ProcessCommand(string[] input)
+        {
+            var command = InputParser.ParseCommand(input);
+            if (command != Command.Place && ToyRobot.Position == null) return string.Empty;
+
+            if (command == Command.Place && input.Length < 3)
+            {
+                string[] placeParams = input[1].Split(',');
+                if (placeParams.Length < 3 && PlaceDirection != string.Empty)
+                {
+                    input[1] = input[1] + ',' + PlaceDirection;
+                }
+                else
+                {
+                    PlaceDirection = placeParams[2];
+                }
+            }
+
+            switch (command)
+            {
+                case Command.Place:
+                    var placeCommandParameter = InputParser.ParseCommandParameter(input);
+                    if (SquareBoard.IsValidPosition(placeCommandParameter.Position))
+                        ToyRobot.Place(placeCommandParameter.Position, placeCommandParameter.Direction);
+                    break;
+                case Command.Move:
+                    var newPosition = ToyRobot.GetNextPosition();
+                    if (SquareBoard.IsValidPosition(newPosition))
+                        ToyRobot.Position = newPosition;
+                    break;
+                case Command.Left:
+                    ToyRobot.RotateLeft();
+                    break;
+                case Command.Right:
+                    ToyRobot.RotateRight();
+                    break;
+                case Command.Report:
+                    return GetReport();
+            }
+            return string.Empty;
+        }
+
+        public string GetReport()
+        {
+            return string.Format("Output: {0},{1},{2}", ToyRobot.Position.X,
+                ToyRobot.Position.Y, ToyRobot.Direction.ToString().ToUpper());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿
+
+namespace ToyRobort
+{
+    public enum Command
+    {
+        Place,
+        Move,
+        Left,
+        Right,
+        Report
+    }
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+ 
+    public class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            const string description =
+@" 
+
+  1: Place the toy on a 6 x 6 grid
+     using the following command:
+
+     PLACE X,Y,Direction
+
+     PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST
+
+  2: When the toy is placed, use the following commands to move the position of toy.
+                
+     REPORT – REPORT will announce the X,Y and Direction of the robot. 
+     LEFT   – turns the toy 90 degrees left.
+     RIGHT  – turns the toy 90 degrees right.
+     MOVE   – will move the toy robot one unit forward in the direction it is currently facing.
+     END    - End the application
+";
+
+            ToyBoard squareBoard = new ToyBoard(6, 6);
+            InputParser inputParser = new InputParser();
+            ToyRobot robot = new ToyRobot();
+            var simulator = new Behaviour(robot, squareBoard, inputParser);
+
+            var stopApplication = false;
+            Console.WriteLine(description);
+            do
+            {
+                var command = Console.ReadLine();
+                if (command == null) continue;
+
+                if (command.Equals("END"))
+                    stopApplication = true;
+                else
+                {
+                    try
+                    {
+                        var output = simulator.ProcessCommand(command.Split(' '));
+                        if (!string.IsNullOrEmpty(output))
+                            Console.WriteLine(output);
+                    }
+                    catch (ArgumentException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            } while (!stopApplication);
+        }
+    }
+
+    public class ToyBoard
+    {
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public ToyBoard(int rows, int columns)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+        }
+
+        public bool IsValidPosition(Position position)
+        {
+            return position.X < Columns && position.X >= 0 &&
+                   position.Y < Rows && position.Y >= 0;
+        }
+    }
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    public class InputParser 
+    {
+        // this method takes a string array and compares the first element to the list of commands
+        // if the command doesn't exist then an error is thrown, otherwise the command is returned
+        public Command ParseCommand(string[] rawInput)
+        {
+            Command command;
+            if (!Enum.TryParse(rawInput[0], true, out command))
+                throw new ArgumentException("Sorry, your command was not recognised. Please try again using the following format: PLACE X,Y,Direction|MOVE|LEFT|RIGHT|REPORT");
+            return command;
+        }
+
+        // Extracts the parameters from the user input and returns it       
+        public PlaceCommandParameter ParseCommandParameter(string[] input)
+        {
+            var thisPlaceCommandParameter = new PlaceCommandParameterParser();
+            return thisPlaceCommandParameter.ParseParameters(input);
+        }
+    }
+
+    public class PlaceCommandParameter
+    {
+        public Position Position { get; set; }
+        public Direction Direction { get; set; }
+
+        public PlaceCommandParameter(Position position, Direction direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+    }
+
+    // This class checks the parameters for the "PLACE" command.
+    public class PlaceCommandParameterParser
+    {
+        // Number of parameters provided for "PLACE" Command. (X,Y,F)
+        private const int ParameterCount = 3;
+
+        // Number of raw input items expected from user.
+        private const int CommandInputCount = 2;
+
+        // Checks the toy's initial position and the direction it is going to be facing.
+        public PlaceCommandParameter ParseParameters(string[] input)
+        {
+            Direction direction;
+            Position position = null;
+
+            // Checks that Place command is followed by valid command parameters (X,Y and F toy's face direction).
+            if (input.Length != CommandInputCount)
+                throw new ArgumentException("Incomplete command. Please ensure that the PLACE command is using format: PLACE X,Y,Direction");
+
+            // Checks that three command parameters are provided for the PLACE command.   
+            var commandParams = input[1].Split(',');
+            if (commandParams.Length != ParameterCount)
+                throw new ArgumentException("Incomplete command. Please ensure that the PLACE command is using format: PLACE X,Y,Direction");
+
+            // Checks the direction which the toy is going to be facing.
+            if (!Enum.TryParse(commandParams[commandParams.Length - 1], true, out direction))
+                throw new ArgumentException("Invalid direction. Please select from one of the following directions: NORTH|EAST|SOUTH|WEST");
+
+            var x = Convert.ToInt32(commandParams[0]);
+            var y = Convert.ToInt32(commandParams[1]);
+            position = new Position(x, y);
+
+            return new PlaceCommandParameter(position, direction);
+        }
+    }
+
+
+}
